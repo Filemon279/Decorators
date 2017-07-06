8beb9d5de3a74261b4164cfc5cc677dbe768ffd1
Filemon279 <280202@uwr.edu.pl>
2017-07-06 15:03:34 +0200

    Przepisane testy od nowa, indywidualne i dokładniejsze

--- V3.0/tests/test.js
+++ V3.0/tests/test.js
@@ -3,87 +3,212 @@ var equal = require('chai').equale
 var myCode = require('../main')
 let chai = require('chai');
 let should = chai.should();
-var chaiAsPromised = require("chai-as-promised");
-chai.use(chaiAsPromised);
 
 function flatten(arr) {
+	if(Array.isArray(arr)){
   return arr.reduce(function (flat, toFlatten) {
     return flat.concat(Array.isArray(toFlatten) ? flatten(toFlatten) : toFlatten);
   }, []);
+	}
+	else return arr
 }
 
 
-function deepEqual(a, b) {
-    try {
-      assert.deepEqual(a, b);
-    } catch (error) {
-      if (error.name === "AssertionError") return false;
-
-      throw error;
-    }
-    return true;
-};
-
-
-// Then either:
 var expect = chai.expect;
-// or:
 var assert = chai.assert;
 
 describe('test', function(){
 
-//Declare input and expected values
-	var tests = 
-	[
-	//Those one are parsed by SimpleGramma
-	{arg: "00.100%"									,output: [{ value: 0.100, decorators: [ 'iri6' ] }]}, //
-	{arg: "200 zł"									,output: [{ value: 200, decorators: [ 'iri129' ] }]},
-	{arg: "400 zł"									,output: [{ value: 400, decorators: [ 'iri129' ] }]},
-	{arg: "1 kEUR"									,output: [{ value: 1, decorators: [ 'iri2','iri112' ] }]},
-	{arg: "5M"											,output: [{ value: 5, decorators: [ 'iri3/iri12' ] }]},
-	{arg: "7D"											,output: [{ value: 7, decorators: [ 'iri10' ] }]},
-	{arg: "M $5"										,output: [{ value: 5, decorators: [ 'iri3/iri12','iri140',] }]},
-	{arg: "bez ograniczeń"					,output: [{ value: '', decorators: [ 'iri8' ] }]},
-	{arg: "-"												,output: [{ value: '', decorators: [ 'iri9' ] }]},
-	{arg: "$7k"											,output: [{ value: 7, decorators: [ 'iri140','iri2' ] }]},
-	{arg: "3,5%"										,output: [{ value: 3.5, decorators: [ 'iri6' ] }]},
-	{arg: "0,1‰"										,output: [{ value: 0.1, decorators: [ 'iri7' ] }]},
 
-	//Those can't be parsed by SimpleGramma so are parsed by ComplexGramma (with comments, from, to, min. max. etc.) if not found return "[]"
-	{arg: "kwota 234.45 złotych"		,output: [{value:234.45, decorators: ['iri129']},{beforeComment: "kwota"},]},	//Here, I would rather choose word cuting to get 234.45 złotych then making special grammar.
-	{arg: "from 5 to 7 zł"					,output: [{range: "FROM" },{ value: 5 }, { range: "TO" }, { value: 7, decorators: [ "iri129" ]}]},
-	{arg: "0.1%, nie mniej niż 5 zł",output: [{ value: [{value: 0.1,decorators: ["iri6"]}],decorators: ","},{operand: ">="},{value: 5,decorators: ["iri129"]}]},
-	{arg: "2%, min. $1"							,output: [{ value: [{value: 2,decorators: ["iri6"]}],decorators: ","},{operand: ">="},{decorators: ["iri140"], value: 1}]},
-	{arg: "12 zł *)"								,output: [[]]},	
-	{arg: "2% (dla kart w PLN), 3% (dla kart w EUR i USD)"	,output: [{ value: [ { value: 2, decorators: [ "iri6" ] }, { comment: "dla kart w pln" } ], decorators: "," }, { value: 3, decorators: [ "iri6" ] }, { comment: "dla kart w eur i usd" }]},	//Here, I would rather choose word cuting to get 234.45 złotych then making special grammar. AND for many more examples
-	{arg: "50 zł + koszty innych banków"										,output: [{ value: 50, decorators: [ "iri129" ] }, { comment: "+ koszty innych banków" } ]},
-	{arg: "według kosztów rzeczywistych (od 175 do 250 USD)",output: [{comment: "od 175 do 250 usd" }]},
-	{arg: "W Polsce 20 tysięcy zł (w USA 7 500 Dolarów)"		,output: [{ value: 20, decorators: [ 'iri2', 'iri129' ] },{ comment: 'w usa 7 500 dolarów' }]},
-	{arg: "Przykładowy nic nie znaczacy tekst"							,output: [[]]},
-	{arg: "Na chwilę obecną oprocentowanie wynosi nie mniej niż 3%, nie więcej niż 5% (tylko w Frankach)"			,output: [{ value: [{operand: ">="},{value: 3,decorators: ["iri6"]}],decorators: ","},{operand: "<="},{decorators: ["iri6"], value: 5},{comment: "tylko w frankach"}]},
-	{arg: "4 000 000 zł"										,output: [{ value: 4000000, decorators: [ 'iri129' ] }]},
-	{arg: "200 000.34 k euro"										,output: [{ value: 200000.34, decorators: [ 'iri2', 'iri112' ] }]},
+		it('Test for: 00.100%', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators(" 00.100%"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(0.1)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri6")
+		})
 
+		it('Test for: 200 zł', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("200 zł"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(200)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri129")
+		})
 
-	];
+		it('Test for: 400 zł', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("400 zł"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(400)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri129")
+		})
 
+		it('Test for: 1 kEUR', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("1 kEUR"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(1)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri2")
+		    expect(output.decorators).to.include("iri112")
+		})
 
+		it('Test for: 5M', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("5M"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(5)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri3/iri12")
+		})
 
-    describe('Testing enrichValueWithDecorators(value)', function(){
-    tests.forEach(function(test)
-    {
-        it('Test passed: ' +test.arg, function(){
-            output=myCode.enrichValueWithDecorators(test.arg)
-            //console.log(output," : ",flatten(test.output))
-						if(output.length>=1) flatten(output).should.eql(flatten(test.output))//," |",output," : ",test.output).should.eql(true)//console.log(test.output.some(function(item){console.log("O: ",output.some(function(i){true}))}))
-						else output.should.eql(test.output[0])	
-        })
-    });
+		it('Test for: 7D', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("7D"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(7)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri10")
+		})
 
-    })
+		it('Test for: M $5', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("M $5"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(5)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri3/iri12")
+		    expect(output.decorators).to.include("iri140")
+		})
+
+		it('Test for: bez ograniczeń', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("bez ograniczeń"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql('')
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri8")
+		})
+
+		it('Test for: -', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("-"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql('')
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri9")
+		})
+
+		it('Test for: $7k', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("$7k"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(7)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri140")
+		    expect(output.decorators).to.include("iri2")
+		})
+
+		it('Test for: 3,5%', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("3,5%"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(3.5)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri6")
+		})
+
+		it('Test for: 0,1%', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("0,1%"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(0.1)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri6")
+		})
+
+		it('Test for: kwota 234.45 złotych', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("kwota 234.45 złotych"))
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(234.45)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri129")
+		})
+
+		it('Test for: from 5 to 7 zł', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("from 5 to 7 zł"))
+		    expect(output).to.be.an("Array")
+		    expect(output).to.deep.include({range: 'FROM'})
+		    expect(output).to.deep.include({value: 5})
+		    expect(output).to.deep.include({range: 'TO'})
+		    expect(output).to.deep.include({value: 7, decorators: ["iri129"]})
+		})
+
+		it('Test for: 0.1%, nie mniej niż 5 zł', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("0.1%, nie mniej niż 5 zł"))
+		    expect(output).to.be.an("Array")
+		    
+		    expect(output).to.deep.include({value: [{value: 0.1, decorators: ["iri6"]}],decorators: ','})
+		    expect(output).to.deep.include({operand: '>='})
+		    expect(output).to.deep.include({value: 5, decorators: ["iri129"]})
+		})
+
+		it('2%, min. $1', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("2%, min. $1"))
+		    expect(output).to.be.an("Array")
+		    expect(output).to.deep.include({value: [{value: 2, decorators: ["iri6"]}],decorators: ','})
+		    expect(output).to.deep.include({operand: '>='})
+		    expect(output).to.deep.include({value: 1, decorators: ["iri140"]})
+		})
+
+		it('12 zł *)', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("12 zł *)"))
+		    expect(output).to.be.eql([])
+		})
+
+		it('2% (dla kart w PLN), 3% (dla kart w EUR i USD)', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("2% (dla kart w PLN), 3% (dla kart w EUR i USD)"))
+		    expect(output).to.be.an("Array")
+		    expect(output).to.deep.include(({value: [{value: 2, decorators: ["iri6"]}, {comment: "dla kart w pln"} ],decorators: ','}))
+		    expect(output).to.deep.include(({value: 3, decorators: ["iri6"]}, {comment: "dla kart w eur i usd"}))
+		})
+
+		it('50 zł + koszty innych banków', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("50 zł + koszty innych banków"))
+		    expect(output).to.be.an("Array")
+		    expect(output).to.deep.include(({value: 50, decorators: ["iri129"]}, {comment: "+ koszty innych banków"}))
+		})
+
+		it('według kosztów rzeczywistych (od 175 do 250 USD)', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("według kosztów rzeczywistych (od 175 do 250 USD)"))
+		    expect(output).to.be.eql({comment: "od 175 do 250 usd"})
+		})
+
+		it('W Polsce 20 tysięcy zł (w USA 7 500 Dolarów)', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("W Polsce 20 tysięcy zł (w USA 7 500 Dolarów)"))
+		    expect(output).to.be.an("Array")
+		    expect(output).to.deep.include(({value: 20, decorators: ["iri2","iri129"]}, {comment: "w usa 7 500 dolarów"}))
+		})
+
+		it('Przykładowy nic nie znaczacy tekst', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("Przykładowy nic nie znaczacy tekst"))
+		    expect(output).to.be.eql([])
+		})
+
+		it('Na chwilę obecną oprocentowanie wynosi nie mniej niż 3%, nie więcej niż 5% (tylko w Frankach)', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("Na chwilę obecną oprocentowanie wynosi nie mniej niż 3%, nie więcej niż 5% (tylko w Frankach)"))
+		    expect(output).to.be.an("Array")
+		    expect(output).to.deep.include(({value: [{operand: ">="},{value: 3, decorators: ["iri6"]}], decorators: ","}))
+		    expect(output).to.deep.include(({operand: "<="},{value: 5, decorators: ["iri6"]},{comment:"tylko w frankach"}))
+		})
+
+		it('4 000 000 zł', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("4 000 000 zł"))  
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(4000000)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri129")
+		})
+
+		it('200 000.34 k euro', function(){
+		    var output=flatten(myCode.enrichValueWithDecorators("200 000.34 k euro"))  
+		    expect(output).to.have.own.property("value")
+		    expect(output.value).to.eql(200000.34)
+		    expect(output).to.have.own.property("decorators")
+		    expect(output.decorators).to.include("iri112")
+		    expect(output.decorators).to.include("iri2")
+		})
+
 })
-
-
-
-//{ value: 20, decorators: [ 'iri2', 'iri129' ] },{ comment: 'w usa 7 500 dolarów' },{ beforeComment: 'w polsce' }
-//{ value: 20, decorators: [ 'iri2', 'iri129' ] },{ comment: 'w usa 7 500 dolarów' },{ beforeComment: 'w polsce' }
